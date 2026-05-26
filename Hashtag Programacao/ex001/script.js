9d0ea87df5c271af81f5f0be47d08ef4573e5bb8
Bruninhowt <bf601584@gmail.com>
2026-05-26 19:53:50 -0300

    commit

---
+++ Hashtag Programacao/ex001/script.js
@@ -0,0 +1,17 @@
+let imagem = document.querySelector('.imagem');
+let nome_musica = document.getElementById('nome-musica');
+let nome_banda = document.getElementById('nome-banda');
+let heart = document.querySelector('.heart');
+let progresso = document.querySelector('.progresso');
+let inicio = document.querySelector('.inicio');
+let fim = document.querySelector('.fim');
+let embaralhar = document.querySelector('#embaralhar');
+let anterior = document.querySelector('#anterior');
+let play = document.querySelector('#play');
+let next = document.querySelector('#next');
+let repetir = document.querySelector('#repetir');
+let audio = document.getElementById('audio')
+
+function play(){
+    
+}
